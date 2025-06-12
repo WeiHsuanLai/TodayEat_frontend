ec028d9dd1d8cff0a5a7c3fca2fd929a9fa603ce
leolai <leolai@sunsky.com.tw>
2025-06-12 11:57:11 +0800

    更新關於我們

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page class="q-pa-md">
-    <div class="text-h5">{{ t('about.aboutus') }}</div>
+    <div class="text-h5">關於我們</div>
     <div class="md-hide">這段會在 md 時隱藏</div>
     <div class="q-pa-md bg-primary text-white text-h5">測試 Quasar 样式</div>
   </q-page>
 </template>
 <script setup lang="ts">
-import { useI18n } from 'vue-i18n';
+// import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n();
+// const { t } = useI18n();
 </script>
