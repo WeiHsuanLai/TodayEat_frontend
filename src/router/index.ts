2674107724062ce468449abb809b5b3b7cab7327
leolai <leolai@sunsky.com.tw>
2025-07-02 11:42:38 +0800

    強制使用history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,13 @@
 import { defineRouter } from '#q-app/wrappers';
-import {
-  createMemoryHistory,
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from 'vue-router';
+import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router';
 import routes from './routes';
 import { authGuard } from './guards/authGuard';
 
 export default defineRouter(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === 'history'
-      ? createWebHistory
-      : createWebHashHistory;
-
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
-    history: createHistory(process.env.VUE_ROUTER_BASE),
+    history: process.env.SERVER ? createMemoryHistory() : createWebHistory(),
   });
 
   // 登入驗證
