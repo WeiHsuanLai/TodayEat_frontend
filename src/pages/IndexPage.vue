6c43df89115a84114d3524e733ea391fd5a25ce3
leolai <leolai@sunsky.com.tw>
2025-07-24 15:13:47 +0800

    先重設UIStore

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,16 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <!-- <h1>123</h1> -->
-    <GridLottery :key="uiStore.gridLotteryKey" />
-    <!-- <h1>123</h1> -->
+    <!-- <GridLottery :key="uiStore.gridLotteryKey" /> -->
+    <GridLottery />
   </q-page>
 </template>
 
 <script setup lang="ts">
 import GridLottery from '../components/GridLottery.vue';
-import { useUIStore } from 'stores/ui';
+// import { useUIStore } from 'stores/ui';
 // import { useI18n } from 'vue-i18n';
 // const { t } = useI18n();
 
-const uiStore = useUIStore();
+// const uiStore = useUIStore();
 // 提供一個方法讓父層可以重設它（稍後會用）
 </script>
